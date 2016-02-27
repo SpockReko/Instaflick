5d8d8209f8ff96b316edaf67b62facf640bb69a9
pontusthome <pontusthome@gmail.com>
2016-02-27 13:28:01 +0100

    Added IDAO AbstractDAO IInstaFlick, IInstaFlickUserRegistry InstaFlickUserRegistry

--- InstaFlickr/src/main/java/se/webapp/instaflickr/model/InstaFlick.java
+++ InstaFlickr/src/main/java/se/webapp/instaflickr/model/InstaFlick.java
@@ -5,10 +5,28 @@
  */
 package se.webapp.instaflickr.model;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
 /**
  *
- * @author TH
+ * @author Pontus
  */
-public class InstaFlick {
+@ApplicationScoped
+public class InstaFlick{
+
+    public InstaFlick() {
+        Logger.getAnonymousLogger().log(Level.INFO, "InstaFlick is alive");
+    }
+ 
+    @EJB
+    private IInstaFlickUserRegistry userRegistry;
     
+    public IInstaFlickUserRegistry getUserRegistry() {
+        return userRegistry;
+    }
+
 }
