f4c70ecf05a394448fed586410e683b887dd1586
cpupro <practical_master@hotmail.com>
2016-02-27 18:00:40 +0100

    Added (hopefully) working grid that uses data from a controller instead of hardcoded HTML.

--- InstaFlickr/src/main/webapp/instaflickr/app/js/controllers.js
+++ InstaFlickr/src/main/webapp/instaflickr/app/js/controllers.js
@@ -12,7 +12,77 @@ instaFlickControllers.controller('ProfileCtrl', ['$scope',
         var imageData = ['http://placehold.it/400x400', 'http://placehold.it/400x400',
             'http://placehold.it/400x400', 'http://placehold.it/400x400', 'http://placehold.it/400x400',
             'http://placehold.it/400x400', 'http://placehold.it/400x400', 'http://placehold.it/400x400'];
-        
-        $scope.images = imageData;
 
+        // $scope.images = imageData;
+
+        var testData = [
+            {
+                "_id": 1,
+                "type": "image"
+            },
+            {
+                "_id": 2,
+                "type": "image"
+            },
+            {
+                "_id": 3,
+                "type": "album",
+                "alb": [
+                    {
+                        "_id": 4,
+                        "type": "image"
+                    },
+                    {
+                        "_id": 5,
+                        "type": "image"
+                    },
+                    {
+                        "_id": 6,
+                        "type": "image"
+                    }
+                ]
+            },
+            {
+                "_id": 7,
+                "type": "image"
+            },
+            {
+                "_id": 8,
+                "type": "image"
+            },
+            {
+                "_id": 9,
+                "type": "image"
+            },
+            {
+                "_id": 10,
+                "type": "album",
+                "alb": [
+                    {
+                        "_id": 11,
+                        "type": "image"
+                    },
+                    {
+                        "_id": 12,
+                        "type": "image"
+                    }
+                ]
+            },
+            {
+                "_id": 13,
+                "type": "image"
+            },
+            {
+                "_id": 14,
+                "type": "image"
+            },
+            {
+                "_id": 15,
+                "type": "image"
+            }
+        ];
+
+        console.log(testData);
+
+        $scope.testData = testData;
     }]);
