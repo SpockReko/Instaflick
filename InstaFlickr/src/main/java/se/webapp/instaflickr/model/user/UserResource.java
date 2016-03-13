db85398cd3b56e9e292efa0d3a8e3279f2d6f5a4
cpupro <practical_master@hotmail.com>
2016-03-13 19:42:50 +0100

    Logging modified for UserResource

--- InstaFlickr/src/main/java/se/webapp/instaflickr/model/user/UserResource.java
+++ InstaFlickr/src/main/java/se/webapp/instaflickr/model/user/UserResource.java
@@ -36,77 +36,12 @@ public class UserResource {
 
     private static final Logger LOG = Logger.getLogger(UserResource.class.getName());
 
-    @GET
-    @Path("userprofile")
-    public Response getUserProfile(@QueryParam("username") String username) {
-        LOG.log(Level.INFO, "getUserProfile(): " + username);
-
-        InstaFlickUser user = instaFlick.getUserRegistry().find(username);
-        LOG.log(Level.INFO, "got user: " + user.getUsername());
-        JsonObjectBuilder builder = Json.createObjectBuilder();
-
-        builder.add("username", user.getUsername());
-        builder.add("email", user.getEmail());
-        builder.add("fname", user.getFname());
-        builder.add("lname", user.getLname());
-        builder.add("description", user.getDescription());
-
-        return Response.ok(builder.build()).build();
-    }
-
-    @GET
-    @Path(value = "session")
-    public Response getSession() {
-        LOG.warning("Getting the session");
-        if (!session.getSession()) {
-            return Response.status(Response.Status.NOT_ACCEPTABLE).build();
-        }
-
-        String username = session.getSessionID();
-        JsonObject value = Json.createObjectBuilder().add("username", username).build();
-        return Response.ok(value).build();
-    }
-
-    @GET
-    @Path(value = "logout")
-    public Response logout() {
-        LOG.warning("Logging out");
-        session.setSession(false);
-        session.setSessionID(null);
-        return Response.ok().build();
-    }
-
-    @GET
-    @Path(value = "loggedIn")
-    public Response isLoggedIn() {
-        LOG.warning("Checking session");
-        JsonObject value = Json.createObjectBuilder().add("loggedIn", session.getSession()).build();
-        return Response.ok(value).build();
-    }
-
-    @GET
-    public Response login(@QueryParam(value = "username") String username,
-            @QueryParam(value = "password") String password) {
-        LOG.warning("User trying to log in: " + username + " " + password);
-        InstaFlickUser user = instaFlick.getUserRegistry().find(username);
-        if (user == null) {
-            return Response.status(Response.Status.CONFLICT).build();
-        }
-        if (!user.getPassword().equals(password)) {
-            return Response.status(Response.Status.NOT_ACCEPTABLE).build();
-        }
-
-        session.setSession(true);
-        session.setSessionID(username);
-        return Response.status(Response.Status.ACCEPTED).build();
-    }
-
     @POST
     public Response create(@QueryParam(value = "username") String username,
             @QueryParam(value = "password") String password,
             @QueryParam(value = "repeatPassword") String repeatPassword) {
-        LOG.log(Level.INFO, "Insert {0} {1}", new Object[]{username, password, repeatPassword});
-        LOG.warning("Creating new user " + username + " " + password + " " + repeatPassword);
+        LOG.log(Level.INFO, "\t Creating new user {0} {1} {2}", new Object[]{username, password, repeatPassword});
+
         InstaFlickUser exists;
         try {
             exists = instaFlick.getUserRegistry().find(username);
@@ -129,7 +64,7 @@ public class UserResource {
             URI uri = uriInfo.getAbsolutePathBuilder()
                     .path(String.valueOf(user.getUsername()))
                     .build(new UserWrapper(user));
-            LOG.warning("URI " + uri.toString());
+            LOG.log(Level.INFO, "URI " + uri.toString());
             session.setSession(true);
             session.setSessionID(username);
             return Response.created(uri).build();
@@ -144,11 +79,11 @@ public class UserResource {
             @QueryParam(value = "fname") String fname,
             @QueryParam(value = "lname") String lname,
             @QueryParam(value = "description") String description) {
-        LOG.warning("Setting up profile: " + email + " " + fname + " " + lname + " " + description);
+        LOG.log(Level.WARNING, "Setting up profile: {0} {1} {2} {3}", new Object[]{email, fname, lname, description});
 
         String username = session.getSessionID();
         InstaFlickUser user = instaFlick.getUserRegistry().find(username);
-        LOG.warning("Setting up user: " + user.getUsername());
+        LOG.log(Level.INFO, "\t Setting up user: " + user.getUsername());
 
         user.setEmail(email);
         user.setFname(fname);
@@ -165,12 +100,12 @@ public class UserResource {
             @QueryParam(value = "fname") String fname,
             @QueryParam(value = "lname") String lname,
             @QueryParam(value = "description") String description) {
-
-        LOG.log(Level.INFO, "Update called" + lname);
+        LOG.log(Level.INFO, "Updating profile email: {0}, fname: {1}, lname: {2}, description: {3}", new Object[]{email, fname, lname, description});
 
         String username = session.getSessionID();
         InstaFlickUser user = instaFlick.getUserRegistry().find(username);
-        LOG.warning("Setting up user: " + user.getUsername());
+
+        LOG.log(Level.INFO, "\t Updating profile for user {0}", user.getUsername());
 
         user.setEmail(email);
         user.setFname(fname);
@@ -180,4 +115,69 @@ public class UserResource {
 
         return Response.ok().build();
     }
+
+    @GET
+    @Path("userprofile")
+    public Response getUserProfile(@QueryParam("username") String username) {
+        LOG.log(Level.INFO, "Getting profile info for {0}", username);
+
+        InstaFlickUser user = instaFlick.getUserRegistry().find(username);
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+
+        builder.add("username", user.getUsername());
+        builder.add("email", user.getEmail());
+        builder.add("fname", user.getFname());
+        builder.add("lname", user.getLname());
+        builder.add("description", user.getDescription());
+
+        return Response.ok(builder.build()).build();
+    }
+
+    @GET
+    @Path(value = "session")
+    public Response getSession() {
+        LOG.log(Level.INFO, "Getting the client session");
+        if (!session.getSession()) {
+            return Response.status(Response.Status.NOT_ACCEPTABLE).build();
+        }
+
+        String username = session.getSessionID();
+        JsonObject value = Json.createObjectBuilder().add("username", username).build();
+        return Response.ok(value).build();
+    }
+
+    @GET
+    public Response login(@QueryParam(value = "username") String username,
+            @QueryParam(value = "password") String password) {
+        LOG.log(Level.INFO, "{0} trying to log in with password {1}", new Object[]{username, password});
+        InstaFlickUser user = instaFlick.getUserRegistry().find(username);
+        if (user == null) {
+            return Response.status(Response.Status.CONFLICT).build();
+        }
+        if (!user.getPassword().equals(password)) {
+            return Response.status(Response.Status.NOT_ACCEPTABLE).build();
+        }
+
+        session.setSession(true);
+        session.setSessionID(username);
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
+
+    @GET
+    @Path(value = "logout")
+    public Response logout() {
+        LOG.log(Level.INFO, "Logging out");
+        session.setSession(false);
+        session.setSessionID(null);
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path(value = "loggedIn")
+    public Response isLoggedIn() {
+        LOG.log(Level.INFO, "Checking session");
+        JsonObject value = Json.createObjectBuilder().add("loggedIn", session.getSession()).build();
+        return Response.ok(value).build();
+    }
+
 }
